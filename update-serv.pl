1eb3d3da3fddfacf17fd430d441e05d75b5e5771
JozanLeClerc <bousset.rudy@gmail.com>
2020-07-06 16:51:19 +0200

    Scripts

---
+++ update-serv.pl
@@ -0,0 +1,23 @@
+#!/usr/local/bin/perl
+
+use strict;
+use warnings;
+use Term::ANSIColor;
+
+sub main {
+	system(
+		'/usr/local/bin/dash',
+		'-c',
+		'git -C /usr/local/www/jozan pull >/dev/null 2>&1'
+		);
+	system(
+		'/usr/local/bin/dash',
+		'-c',
+		'git -C /root/scripts pull >/dev/null 2>&1'
+		);
+	exit;
+}
+
+main();
+
+__END__
--- update-site.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/local/bin/dash
-
-git -C /usr/local/www/jozan pull
-git -C /root/scripts pull
-exit
