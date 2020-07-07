ce70c062c6e2b796456cd548f2f1254b82b54d3b
JozanLeClerc <bousset.rudy@gmail.com>
2020-07-07 20:04:31 +0200

    Good start

--- update-gitjoe.pl
+++ update-gitjoe.pl
@@ -5,6 +5,22 @@ use warnings;
 use Term::ANSIColor;
 
 sub main {
+	my $homedir = '/usr/home/';
+	my @users;
+	opendir(DIR, $homedir);
+	my $i = 0;
+	while (my $dir = readdir(DIR)) {
+		next if ($dir eq 'git-ro');
+		next if ($dir =~ m/^\./);
+		$users[$i] = $dir;
+		$i += 1;
+	}
+	closedir(DIR);
+	$i = 0;
+	while ($i < @users) {
+		print $users[$i] . "\n";
+		$i += 1;
+	}
 	exit;
 }
 
