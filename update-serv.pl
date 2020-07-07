475715b358a2a05fec4dab403f4b41d605fdb4c5
JozanLeClerc <bousset.rudy@gmail.com>
2020-07-07 18:21:18 +0200

    Update serv now updates gitjoe

--- update-serv.pl
+++ update-serv.pl
@@ -13,6 +13,11 @@ sub main {
 	system(
 		'/usr/local/bin/dash',
 		'-c',
+		'git -C /usr/local/www/git-jozan pull >/dev/null 2>&1'
+		);
+	system(
+		'/usr/local/bin/dash',
+		'-c',
 		'git -C /root/scripts pull >/dev/null 2>&1'
 		);
 	exit;
