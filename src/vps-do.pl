c298904d9d9a12281b368dd180f3e8b4f7aba898
JozanLeClerc <bousset.rudy@gmail.com>
2020-07-08 17:41:28 +0200

    Tree update

--- src/vps-do.pl
+++ src/vps-do.pl
@@ -18,10 +18,10 @@ sub main {
 	my $ssh_boy = 'root@jozanleclerc.xyz';
 	if (
 		$ARGV[0] eq 'addsshkey' ||
+		$ARGV[0] eq 'adduser' ||
 		$ARGV[0] eq 'chdesc' ||
 		$ARGV[0] eq 'chowner' ||
 		$ARGV[0] eq 'newrepo' ||
-		$ARGV[0] eq 'newuser' ||
 		$ARGV[0] eq 'rmrepo' ||
 		$ARGV[0] eq 'rmuser'
 		) {
@@ -40,17 +40,36 @@ sub main {
 			. colored($ARGV[0], 'bold yellow')
 			. ": unknown script. Known scripts are:\n"
 			. colored("addsshkey\n", 'bold green')
+			. colored("adduser\n", 'bold green')
 			. colored("chdesc\n", 'bold green')
 			. colored("chowner\n", 'bold green')
 			. colored("newrepo\n", 'bold green')
-			. colored("newuser\n", 'bold green')
 			. colored("rmrepo\n", 'bold green')
 			. colored("rmuser\n", 'bold green')
 			. colored("update-gitjoe\n", 'bold green')
 			. colored("update-vps\n", 'bold green');
 		exit 2;
 	}
-	print "Calling " . colored($called_script, 'bold green') . " via " . colored($ssh_boy, 'bold') . "...\n";
+	print "Calling " . colored($called_script, 'bold green') . " via " . colored($ssh_boy, 'bold magenta') . ".\n";
+	if ($argc > 1) {
+		print "Arguments:\n";
+		my $i = 1;
+		while ($i < $argc) {
+			print colored($ARGV[$i], 'bold yellow') . "\n";
+			$i += 1;
+		}
+	}
+	my $dash = `/bin/sh -c "which dash"`;
+	chomp $dash;
+	system(
+		$dash,
+		'-c',
+		'ssh ' . $ssh_boy . " << EOF 2>&1
+ls -lh
+uname -n
+exit
+"
+		);
 	exit;
 }
 
