5fbd3e4a4156a2c61d7f6e9a953c8dd0f4b1b341
JozanLeClerc <bousset.rudy@gmail.com>
2022-04-21 22:17:18 +0200

    counter update, less retarded and probably opti

--- src/cron/counter.sh
+++ src/cron/counter.sh
@@ -3,10 +3,7 @@
 logfile='/var/log/nginx/access.log'
 destfile='/usr/local/www/jozan/index.html'
 foretext='Unique visitors: '
-tmp=$(mktemp)
 
 [ -e $logfile ]  || exit 1
 [ -e $destfile ] || exit 1
-sed "s/$foretext.*$/$foretext<b>$(awk '{print $1}' $logfile | sort | uniq | wc -l | tr -d ' ')<\/b>/" $destfile >"$tmp" || rm "$tmp"
-cat "$tmp" >$destfile
-rm "$tmp"
+sed -i '' "s/$foretext.*$/$foretext<b>$(awk '{print $1}' $logfile | sort | uniq | wc -l | tr -d ' ')<\/b>/" $destfile
